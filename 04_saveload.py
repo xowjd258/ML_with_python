e2334d0254129efb7bcd6105b582e16c532ca688
xowjd258 <55902342+xowjd258@users.noreply.github.com>
2019-10-31 14:42:18 +0900

    Add files via upload

---
+++ 04_saveload.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import torch
+import torchvision.datasets as dsets
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import random
+
+
+# In[1]:
+
+
+# parameters
+learning_rate = 0.001
+training_epochs = 15
+batch_size = 100
+
+# MNIST dataset
+mnist_train = dsets.MNIST(root='MNIST_data/',
+                          train=True,
+                          transform=transforms.ToTensor(),
+                          download=True)
+
+mnist_test = dsets.MNIST(root='MNIST_data/',
+                         train=False,
+                         transform=transforms.ToTensor(),
+                         download=True)
+
+# dataset loader
+data_loader = torch.utils.data.DataLoader(dataset=mnist_train,
+                                          batch_size=batch_size,
+                                          shuffle=True,
+                                          drop_last=True)
+
+
+# In[2]:
+
+
+# CNN Model (2 conv layers)
+class CNN(torch.nn.Module):
+
+    def __init__(self):
+        super(CNN, self).__init__()
+        # L1 ImgIn shape=(?, 28, 28, 1)
+        #    Conv     -> (?, 28, 28, 32)
+        #    Pool     -> (?, 14, 14, 32)
+        self.layer1 = torch.nn.Sequential(
+            torch.nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1),
+            torch.nn.ReLU(),
+            torch.nn.MaxPool2d(kernel_size=2, stride=2))
+        # L2 ImgIn shape=(?, 14, 14, 32)
+        #    Conv      ->(?, 14, 14, 64)
+        #    Pool      ->(?, 7, 7, 64)
+        self.layer2 = torch.nn.Sequential(
+            torch.nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+            torch.nn.ReLU(),
+            torch.nn.MaxPool2d(kernel_size=2, stride=2))
+        # Final FC 7x7x64 inputs -> 10 outputs
+        self.fc = torch.nn.Linear(7 * 7 * 64, 10, bias=True)
+        torch.nn.init.xavier_uniform_(self.fc.weight)
+
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = out.view(out.size(0), -1)   # Flatten them for FC
+        out = self.fc(out)
+        return out
+
+
+# In[3]:
+
+
+model = CNN().to(device)
+
+# define cost/loss & optimizer
+criterion = torch.nn.CrossEntropyLoss().to(device)    # Softmax is internally computed.
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+total_batch = len(data_loader)
+
+
+# In[4]:
+
+
+for epoch in range(training_epochs):
+    avg_cost = 0
+
+    for X, Y in data_loader:
+        X = X.to(device)
+        Y = Y.to(device)
+
+        optimizer.zero_grad()
+        hypothesis = model(X)
+        cost = criterion(hypothesis, Y)
+        cost.backward()
+        optimizer.step()
+
+        avg_cost += cost / total_batch
+
+    print('Epoch:', '%04d' % (epoch + 1), 'cost =', '{:.9f}'.format(avg_cost))
+
+print('Learning finished')
+
+
+# In[5]:
+
+
+torch.save(model.state_dict(), "./model/model.pth")#도중에 그만두고 싶거나 자기가 원하는 무언가를 세이브하고 싶을 때 ,어느 지점에서 멈출지를 ?
+
+
+# In[6]:
+
+
+new_model = CNN().to(device)
+new_model.load_state_dict(torch.load('./model/model.pth'))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
